bc294a52c14ed4f8749a64ce09d602b1d4e87942
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2020-09-21 14:18:26 -0700

    test commit:

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -1,28 +1,24 @@
 package bot;
 
 
-import bot.database.MongoConnection;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import org.bson.Document;
-import org.bson.conversions.Bson;
-
-import javax.swing.SwingUtilities;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
 
 import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.set;
 
 
 public class Main{
 
-    static String token = "NzA1NTQ0NDA3MTU5NjY4ODQ2.XqtPkQ.5jrd05vt1t_TAA84y6ZOUHotPgo";
+    static String token;
     public static void main(String[] args) throws Exception{
        // System.out.println(Calculator.calculate("(2+10)"));
 
-
-        if(args.length>0){
-            token = args[0];
-        }
+        String path = Main.class.getClassLoader().getResource("bot.secrets").getPath().replace("%20"," ");
+        File f = new File(path);
+        Scanner scanner = new Scanner(new FileInputStream(f));
+        token = scanner.nextLine();
+        System.out.println(token);
 
         new Thread(){
             @Override
