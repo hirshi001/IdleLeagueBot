80b41d29d3f201911f70a788e2f95dec0916b308
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2020-10-19 12:02:33 -0700

    created GameAccount class which will help in accessing the user account related documents form the database

--- src/main/java/bot/gameutil/GameAccount.java
+++ src/main/java/bot/gameutil/GameAccount.java
@@ -1,4 +1,23 @@
 package bot.gameutil;
 
+import bot.database.MongoConnection;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class GameAccount {
+
+
+    public static Document getOneVOne(long id){
+        return get(MongoConnection.getOneVOneBotCollection(),id);
+    }
+
+    public static Document getUser(long id){
+        return get(MongoConnection.getUsersCollection(),id);
+    }
+
+    public static Document get(MongoCollection<Document> coll,  long id){
+        return coll.find(eq("_id",id)).first();
+    }
 }
